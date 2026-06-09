bdaca4cda185cf4969c0f61534014ffd738c2a0b
sunux <sunsteamdj@gmail.com>
2026-06-10 02:10:54 +0800

    fix: quit app when closing window via title bar button

--- Readpic/App/ReadpicApp.swift
+++ Readpic/App/ReadpicApp.swift
@@ -1,7 +1,14 @@
 import SwiftUI
 
+private final class AppDelegate: NSObject, NSApplicationDelegate {
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        true
+    }
+}
+
 @main
 struct ReadpicApp: App {
+    @NSApplicationDelegateAdaptor(AppDelegate.self) private var appDelegate
     @State private var model = ViewerModel()
 
     init() {
